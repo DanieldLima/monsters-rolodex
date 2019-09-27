1efa8bd770c598e5ed0dc59cd6b2e25c414a069e
Daniel Lima <familiasilva.daniel@gmail.com>
2019-09-27 16:42:50 -0300

    Updates

---
+++ precache-manifest.3833aaadb6a5c8c70e9e720ab719e381.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4e77a06f76c1d89d8b544f5ee32d24",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fe2456256c578dcf44d2",
+    "url": "/react-monsters-rolodex/static/css/main.5f281a51.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/react-monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "fe2456256c578dcf44d2",
+    "url": "/react-monsters-rolodex/static/js/main.58dfca5f.chunk.js"
+  },
+  {
+    "revision": "bfb134e8386488a68f2a",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.eb0d3c9f.js"
+  }
+]);
